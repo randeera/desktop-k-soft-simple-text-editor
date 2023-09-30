50108062514cad4ba976ed3ee82cc05554fc3c84
Kaveendra Randeera <khkrandeera@gmail.com>
2023-09-30 23:14:31 +0530

    Add AppInitalizer class

---
+++ src/main/java/lk/ijse/dep11/app/AppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep11.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/TextForm"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Simple Text Editor");
+        primaryStage.show();
+        primaryStage.setResizable(false);
+
+    }
+}
