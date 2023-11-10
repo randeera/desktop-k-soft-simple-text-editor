d3c3347ac6af0438e631661cd0363a724ead1b57
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-11 02:08:51 +0530

    Complete Open menu item btn

--- src/main/java/lk/ijse/dep11/app/AppInitializer.java
+++ src/main/java/lk/ijse/dep11/app/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep11.app;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -9,6 +10,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AppInitializer extends Application {
+    public static SimpleStringProperty observableTitle = new SimpleStringProperty("untitled");
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/lk/ijse/dep11/app/TextFormController.java
+++ src/main/java/lk/ijse/dep11/app/TextFormController.java
@@ -2,14 +2,55 @@ package lk.ijse.dep11.app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 
 public class TextFormController {
 
     public AnchorPane root;
     public HTMLEditor htmlEditor;
+    public MenuBar menuBar;
+    public Menu menuFile;
+    public MenuItem menuItemNew;
+    public MenuItem menuItemOpen;
+    public MenuItem menuItemSave;
+    public MenuItem menuItemSaveAs;
+    public MenuItem menuItemClose;
+    public Menu menuHelp;
+    public MenuItem menuItemAbout;
+    public Label lblFooter;
+
+    public File fileAddress;
+    public TextArea txtBody;
+
+    public void menuItemNewOnAction(ActionEvent actionEvent) {
+
+
+    }
+
+    public void menuItemOpenOnAction(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open a New File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files" , "*.txt", "*.doc", "*.pages", "*.docx", "*.md", "*.wps") );
+        File file = fileChooser.showOpenDialog(txtBody.getScene().getWindow());
+        String fileLocation = file.getAbsolutePath();
+        String fileName = String.valueOf(file);
+        try (FileReader fr = new FileReader(file);
+             BufferedReader br = new BufferedReader(fr)) {
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null){
+                sb.append(line).append("\n");
+            }
+            txtBody.setText(sb.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
