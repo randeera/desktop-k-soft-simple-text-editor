b10b0bcc89a5e1ac931b79a3f06b2700f8eb02ae
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-10 22:08:58 +0530

    Fix the issue for Html editor loading

--- src/main/java/lk/ijse/dep11/app/TextFormController.java
+++ src/main/java/lk/ijse/dep11/app/TextFormController.java
@@ -1,10 +1,15 @@
 package lk.ijse.dep11.app;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.HTMLEditor;
+
 
 public class TextFormController {
 
     public AnchorPane root;
+    public HTMLEditor htmlEditor;
 }
